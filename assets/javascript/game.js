8f96dd5948bcf573e1665433b4e2229e70204194
Julia Snyder <julesanne493@gmail.com>
2020-02-20 16:50:58 -0500

    added instructions and commented code for future reference.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,27 +10,41 @@ var validKeys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 
 function resetGame() {
 
+    // create blank array for the user's guessed letters
     userArray = [];
+    // set tries to 0
     tries = 0;
+    // create blank array to replace letters of the word with dashes
     blankArray = [];
+    //generate a random word from the array of animals
     computerGuess = animals[Math.floor(Math.random() * animals.length)];
+    //split the word into an arry of its individual letters
     computerGuessArray = computerGuess.split("");
-
+    // fill the blank array with dashes equal to the number of letters in the word
     for (var i=0; i<computerGuessArray.length; i++) {
         blankArray.push("-");
     };
+    //make the blank array a string so commas do not appear on the page
     document.getElementById("word").innerHTML = blankArray.join(" ");  
+    // set the guessed letter to nothing
     document.getElementById("user-guess").textContent = " "
+    // set the content of the guessed letters to the user's input
     document.getElementById("user-guesses").textContent = userArray
+    // set the guesses left to be equal to the number of letters in the word
     document.getElementById("guesses-left").textContent = computerGuessArray.length;
 }
 
+// run function when user presses a key
 document.onkeyup = function(event) {  
 
+    // set the input to lower case so it doesn't create an error
     userGuess = event.key.toLowerCase();
     
+    //if user's input is a valid letter and it has not already been guessed
     if (validKeys.indexOf(userGuess)  > -1 && userArray.indexOf(" " + userGuess) === -1) {
+    // add user's guess to the guessed letters array and add a space so it's visually appealing
     userArray.push(" " + userGuess);
+    // reset text content to update
     document.getElementById("user-guess").textContent = userGuess;
     document.getElementById("user-guesses").textContent = userArray;
 
@@ -40,6 +54,8 @@ document.onkeyup = function(event) {
         }
         else {
             for (i=0; i<computerGuessArray.length; i++){
+
+                // if letter with index of i equals user input, set letter index variable to i, reset the blank array's dash at that index to be equal to the user guess, and reload the word to show the correctly guessed letter
                 if (computerGuessArray[i] === userGuess) {
                     var letterIndex = [i];
                     blankArray[letterIndex] = userGuess;
